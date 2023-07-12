4932674079a610851091391d041c6598a0fefaee
Xuechun Hua <hua_xuechun@outlook.com>
2023-07-12 00:08:31 -0700

    setup

--- chatty-backend/src/app.ts
+++ chatty-backend/src/app.ts
@@ -1,12 +1,20 @@
 import express, { Express } from 'express';
 import { ChattyServer } from './setupServer';
+import databaseConnect from './setupDatabase';
+import { config } from './config';
 
 class Application {
-    public initialize(): void {
-        const app: Express = express();
-        const server: ChattyServer = new ChattyServer(app);
-        server.start();
-    }
+  public initialize(): void {
+    this.loadConfig();
+    databaseConnect();
+    const app: Express = express();
+    const server: ChattyServer = new ChattyServer(app);
+    server.start();
+  }
+
+  private loadConfig(): void {
+    config.validateConfig();
+  }
 }
 
 const application: Application = new Application();
